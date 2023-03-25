cbea53bc6463b056178276923bfb429474a21eb7
tzavachil <tzav.achil@yahoo.gr>
2023-03-25 20:53:37 +0200

    Initialize Tabs and Tabs Items' xmls

---
+++ app/src/main/java/com/example/shiftcalendar/ui/summary/tabs/MonthSelectorFragment.java
@@ -0,0 +1,36 @@
+package com.example.shiftcalendar.ui.summary.tabs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shiftcalendar.R;
+
+public class MonthSelectorFragment extends Fragment {
+
+    public MonthSelectorFragment() {
+        // Required empty public constructor
+    }
+
+    public static MonthSelectorFragment newInstance(String param1, String param2) {
+        MonthSelectorFragment fragment = new MonthSelectorFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_month_selector, container, false);
+    }
+}
