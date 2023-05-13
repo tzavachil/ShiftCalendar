e21e86dd1a0850b1282384081777f04bc4d6c2fd
tzavachil <tzav.achil@yahoo.gr>
2023-05-13 18:18:23 +0300

    Complete excel export from MonthSelector

--- app/src/main/java/com/example/shiftcalendar/ui/summary/ExcelCreator.java
+++ app/src/main/java/com/example/shiftcalendar/ui/summary/ExcelCreator.java
@@ -2,18 +2,21 @@ package com.example.shiftcalendar.ui.summary;
 
 import android.app.Activity;
 import android.os.Environment;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.example.shiftcalendar.ShiftDay;
+import com.example.shiftcalendar.ui.summary.tabs.SelectorFragment;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFFont;
@@ -31,6 +34,7 @@ public class ExcelCreator {
 
     private Workbook calendarWorkbook;
     private ArrayList<ShiftDay> data;
+    private ArrayList<ShiftRecyclerData> shifts;
 
     //Cell Styles
     private XSSFCellStyle headerGreenCellStyle;
@@ -39,10 +43,21 @@ public class ExcelCreator {
     private XSSFCellStyle lightGrayCellStyle;
     private XSSFCellStyle centerStyle;
 
+    //Column Max Width
+    private int maxNameSize;
+    private int maxDateSize;
+    private int maxHoursRangeSize;
+    private int maxHoursSize;
+
+    //Sum variables
+    int totalHours;
+    int totalMin;
+
     public ExcelCreator(Activity parentActivity, String month, String year, ArrayList<ShiftDay> data){
 
         this.data = data;
-        Log.d("Debug", "Data Size: " + String.valueOf(data.size()));
+        this.totalHours = 0;
+        this.totalMin = 0;
         this.calendarWorkbook = new XSSFWorkbook();
         this.createExcel(month + " " + year);
         if(this.storeExcelInStorage("SC-" + month + "-" + year)){
@@ -59,7 +74,9 @@ public class ExcelCreator {
         //Row 0 = Data Date Range Header
         Row firstRow = sheet.createRow(row);
         Cell dataDateRangeHeader = firstRow.createCell(0);
-        sheet.addMergedRegion(new CellRangeAddress(row,row,0,3));
+        CellRangeAddress range = new CellRangeAddress(row,row,0,3);
+        this.addBorderToRange(range, sheet);
+        sheet.addMergedRegion(range);
         dataDateRangeHeader.setCellValue(sheetName);
         dataDateRangeHeader.setCellStyle(this.headerGreenCellStyle);
         row++;
@@ -75,10 +92,17 @@ public class ExcelCreator {
         dateHeader.setCellStyle(this.headerYellowCellStyle);
 
         Cell hoursHeader = columnHeaderRow.createCell(2);
-        sheet.addMergedRegion(new CellRangeAddress(row,row,2,3));
+        range = new CellRangeAddress(row,row,2,3);
+        this.addBorderToRange(range, sheet);
+        sheet.addMergedRegion(range);
         hoursHeader.setCellValue("Hours");
         hoursHeader.setCellStyle(this.headerYellowCellStyle);
         row++;
+
+        this.maxNameSize = 0;
+        this.maxDateSize = 0;
+        this.maxHoursRangeSize = 0;
+        this.maxHoursSize = 0;
 
         //Data Row
         this.createDataRows(sheet, row);
@@ -87,7 +111,9 @@ public class ExcelCreator {
         //Row Total Header
         Row totalHeaderRow = sheet.createRow(row);
         Cell totalHeader = totalHeaderRow.createCell(0);
-        sheet.addMergedRegion(new CellRangeAddress(row,row,0,3));
+        range = new CellRangeAddress(row,row,0,3);
+        this.addBorderToRange(range, sheet);
+        sheet.addMergedRegion(range);
         totalHeader.setCellValue("Total");
         totalHeader.setCellStyle(this.headerYellowCellStyle);
         row++;
@@ -96,24 +122,105 @@ public class ExcelCreator {
         Row shiftHoursHeaders = sheet.createRow(row);
 
         Cell shiftsHeader = shiftHoursHeaders.createCell(0);
-        sheet.addMergedRegion(new CellRangeAddress(row, row, 0, 1));
+        range = new CellRangeAddress(row, row, 0, 1);
+        this.addBorderToRange(range, sheet);
+        sheet.addMergedRegion(range);
         shiftsHeader.setCellValue("Shifts");
         shiftsHeader.setCellStyle(this.headerYellowCellStyle);
 
         Cell totalHoursHeader = shiftHoursHeaders.createCell(2);
-        sheet.addMergedRegion(new CellRangeAddress(row, row, 2, 3));
+        range = new CellRangeAddress(row, row, 2, 3);
+        this.addBorderToRange(range, sheet);
+        sheet.addMergedRegion(range);
         totalHoursHeader.setCellValue("Hours");
         totalHoursHeader.setCellStyle(this.headerYellowCellStyle);
         row++;
+
+        this.createTotalRows(sheet, row);
+    }
+
+    private void createTotalRows(Sheet sheet, int startingRow){
+        int row = startingRow;
+
+        SelectorFragment tempSelectorFragment = new SelectorFragment();
+        this.shifts = new ArrayList<>();
+        try {
+            this.shifts = tempSelectorFragment.getShiftsData(this.data);
+            for(ShiftRecyclerData srd : this.shifts){
+                Row currRow = sheet.createRow(row);
+
+                //Shift Name
+                Cell shiftNameCell = currRow.createCell(0);
+                shiftNameCell.setCellValue(srd.getShift().getName());
+                if(srd.getShift().getName().length() > maxNameSize) maxNameSize = srd.getShift().getName().length();
+                shiftNameCell.setCellStyle(this.headerDarkGreyCellStyle);
+
+                //Shift Counter
+                Cell shiftCounterCell = currRow.createCell(1);
+                shiftCounterCell.setCellValue(srd.getCount());
+                shiftCounterCell.setCellStyle(this.lightGrayCellStyle);
+
+                //Shift Hours
+                Cell shiftHoursCell = currRow.createCell(2);
+                CellRangeAddress range = new CellRangeAddress(row, row, 2, 3);
+                this.addBorderToRange(range, sheet);
+                sheet.addMergedRegion(range);
+                shiftHoursCell.setCellValue(this.calculateTotalTime(srd));
+                shiftHoursCell.setCellStyle(this.lightGrayCellStyle);
+
+                row++;
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Row totalRow = sheet.createRow(row);
+
+        //Total Header
+        Cell totalHeader = totalRow.createCell(0);
+        totalHeader.setCellValue("Total");
+        totalHeader.setCellStyle(this.headerDarkGreyCellStyle);
+
+        //Total Shifts Count
+        Cell totalShiftsCount = totalRow.createCell(1);
+        totalShiftsCount.setCellFormula("SUM($B$" + (row - this.shifts.size() + 1) + ":$B$" +   row + ")");
+        totalShiftsCount.setCellStyle(this.headerDarkGreyCellStyle);
+
+        //Total Hours
+        Cell totalShiftHours = totalRow.createCell(2);
+        CellRangeAddress range = new CellRangeAddress(row, row, 2, 3);
+        this.addBorderToRange(range, sheet);
+        sheet.addMergedRegion(range);
+        String totalHoursStr = String.valueOf(this.totalHours);
+        if(totalHoursStr.length() == 1) totalHoursStr = "0" + totalHoursStr;
+        String totalMinStr = String.valueOf(this.totalMin);
+        if(totalMinStr.length() == 1) totalMinStr = "0" + totalMinStr;
+        totalShiftHours.setCellValue(totalHoursStr + ":" + totalMinStr);
+        totalShiftHours.setCellStyle(this.headerDarkGreyCellStyle);
+
+    }
+
+    private String calculateTotalTime(ShiftRecyclerData srd){
+
+        int hours = srd.getHours() + srd.getExtraHours();
+        int min = srd.getMin() + srd.getExtraMin();
+        if(min > 60){
+            hours += min / 60;
+            min += min % 60;
+        }
+        String hoursStr = String.valueOf(hours);
+        if(hoursStr.length() == 1) hoursStr = "0" + hoursStr;
+        String minStr = String.valueOf(min);
+        if(minStr.length() == 1) minStr = "0" + minStr;
+
+        this.totalHours += hours;
+        this.totalMin += min;
+
+        return hoursStr + ":" + minStr;
     }
 
     private void createDataRows(Sheet sheet, int startingRow){
         int row = startingRow;
-
-        int maxNameSize = 0;
-        int maxDateSize = 0;
-        int maxHoursRangeSize = 0;
-        int maxHoursSize = 0;
 
         for(ShiftDay sd : this.data){
             Row currRow = sheet.createRow(row);
@@ -147,10 +254,10 @@ public class ExcelCreator {
         }
 
         //Auto Resize Columns
-        sheet.setColumnWidth(0, this.calculateWidthSize(maxNameSize));
-        sheet.setColumnWidth(1, this.calculateWidthSize(maxDateSize));
-        sheet.setColumnWidth(2, this.calculateWidthSize(maxHoursRangeSize));
-        sheet.setColumnWidth(3, this.calculateWidthSize(maxHoursSize));
+        sheet.setColumnWidth(0, this.calculateWidthSize(this.maxNameSize));
+        sheet.setColumnWidth(1, this.calculateWidthSize(this.maxDateSize));
+        sheet.setColumnWidth(2, this.calculateWidthSize(this.maxHoursRangeSize));
+        sheet.setColumnWidth(3, this.calculateWidthSize(this.maxHoursSize));
     }
 
     private int calculateWidthSize(int maxSize){
@@ -173,32 +280,70 @@ public class ExcelCreator {
     }
 
     private void createCellStyles(){
+
+        XSSFFont defaultFont= (XSSFFont) this.calendarWorkbook.createFont();
+        defaultFont.setFontName("Calibri");
+        defaultFont.setColor(IndexedColors.BLACK.getIndex());
+        defaultFont.setBold(false);
+        defaultFont.setItalic(false);
+
         this.headerGreenCellStyle = (XSSFCellStyle) this.calendarWorkbook.createCellStyle();
         XSSFColor lightGreen = new XSSFColor(this.colorToRgbTable(169,208,142));
         this.headerGreenCellStyle.setFillForegroundColor(lightGreen);
         this.headerGreenCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         this.headerGreenCellStyle.setAlignment(HorizontalAlignment.CENTER);
+        this.addBoldAndItalic(this.headerGreenCellStyle, true, false);
+        this.addBorderToStyle(this.headerGreenCellStyle);
 
         this.headerYellowCellStyle = (XSSFCellStyle) this.calendarWorkbook.createCellStyle();
         XSSFColor lightYellow = new XSSFColor(this.colorToRgbTable(255,217,102));
         this.headerYellowCellStyle.setFillForegroundColor(lightYellow);
         this.headerYellowCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         this.headerYellowCellStyle.setAlignment(HorizontalAlignment.CENTER);
+        this.addBoldAndItalic(this.headerYellowCellStyle, true, true);
+        this.addBorderToStyle(this.headerYellowCellStyle);
 
         this.headerDarkGreyCellStyle = (XSSFCellStyle) this.calendarWorkbook.createCellStyle();
         XSSFColor darkGrey = new XSSFColor(this.colorToRgbTable(128,128,128));
         this.headerDarkGreyCellStyle.setFillForegroundColor(darkGrey);
         this.headerDarkGreyCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         this.headerDarkGreyCellStyle.setAlignment(HorizontalAlignment.CENTER);
+        this.addBoldAndItalic(this.headerDarkGreyCellStyle, true, false);
+        this.addBorderToStyle(this.headerDarkGreyCellStyle);
 
         this.lightGrayCellStyle = (XSSFCellStyle) this.calendarWorkbook.createCellStyle();
         XSSFColor lightGrey = new XSSFColor(this.colorToRgbTable(166,166,166));
         this.lightGrayCellStyle.setFillForegroundColor(lightGrey);
         this.lightGrayCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         this.lightGrayCellStyle.setAlignment(HorizontalAlignment.CENTER);
+        this.addBorderToStyle(this.lightGrayCellStyle);
 
         this.centerStyle = (XSSFCellStyle) this.calendarWorkbook.createCellStyle();
         this.centerStyle.setAlignment(HorizontalAlignment.CENTER);
+        this.addBoldAndItalic(this.centerStyle, false, false);
+        this.addBorderToStyle(this.centerStyle);
+    }
+
+    private void addBoldAndItalic(XSSFCellStyle style, boolean bold, boolean italic){
+        XSSFFont font = (XSSFFont) this.calendarWorkbook.createFont();
+        font.setFontName("Calibri");
+        font.setBold(bold);
+        font.setItalic(italic);
+        style.setFont(font);
+    }
+
+    private void addBorderToStyle(XSSFCellStyle style){
+        style.setBorderTop(BorderStyle.MEDIUM);
+        style.setBorderBottom(BorderStyle.MEDIUM);
+        style.setBorderLeft(BorderStyle.MEDIUM);
+        style.setBorderRight(BorderStyle.MEDIUM);
+    }
+
+    private void addBorderToRange(CellRangeAddress range, Sheet sheet){
+        RegionUtil.setBorderTop(BorderStyle.MEDIUM, range, sheet);
+        RegionUtil.setBorderBottom(BorderStyle.MEDIUM, range, sheet);
+        RegionUtil.setBorderLeft(BorderStyle.MEDIUM, range, sheet);
+        RegionUtil.setBorderRight(BorderStyle.MEDIUM, range, sheet);
     }
 
     private byte[] colorToRgbTable(int r, int g, int b){
